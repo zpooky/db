526f88255c8ecc8a68f6f3df77ce9d4715694f77
Fredrik Olsson <spooky.bender@gmail.com>
2016-10-04 22:29:14 +0200

    much better

--- collection/MpScFifo.h
+++ collection/MpScFifo.h
@@ -16,17 +16,63 @@ namespace sp {
     class MpScFifo {
     private:
         struct Entry;
+        using Entry_t = std::atomic<Entry *>;
 
         struct Entry {
+            virtual std::string to_string() = 0;
+
+            virtual Entry_t &previous() = 0;
+
+            virtual std::string name() = 0;
+
+            virtual ~Entry() {
+            }
+        };
+
+        struct NoEntry : public Entry {
+        private:
+            Entry_t prev;
+        public:
+            NoEntry() : prev{nullptr} {
+
+            }
+
+            std::string to_string() {
+                auto node = prev.load();
+                if (node) {
+                    return node->to_string();
+                }
+                std::string s;
+                return s;
+            }
+
+            std::string name() {
+                return "NoEntry";
+            }
+
+            Entry_t &previous() {
+                return prev;
+            }
+        };
+
+        struct IEntry : public Entry {
         private:
         public:
             T data;
-            std::atomic<Entry *> prev;
+            Entry_t prev;
 
-            explicit Entry(T &&p_data) :
+            explicit IEntry(T &&p_data) :
                     data{std::forward<T>(p_data)},
                     prev{nullptr} {
 
+            }
+
+            Entry_t &previous() {
+                return prev;
+            }
+
+            std::string name() {
+                return "IEntry";
             }
 
             std::string to_string() {
@@ -43,17 +89,17 @@ namespace sp {
             }
         };
 
-        using Entry_t = std::atomic<Entry *>;
-
+/**
+ * m_push <- entryN... <- m_poll
+ */
         Entry_t m_push;
         Entry_t m_poll;
-        Entry *m_reserved;
         //std::atomic<Entry *> m_poll;
     public:
         MpScFifo() :
                 m_push{nullptr},
-                m_poll{nullptr},
-                m_reserved{new Entry({})} {
+                m_poll{nullptr} {
+//            m_reserved->prev.store(m_reserved);
         }
 
         MpScFifo(const MpScFifo &) = delete;
@@ -71,47 +117,62 @@ namespace sp {
         void push_back(D &&data) {
             static_assert(std::is_same<typename std::remove_reference<D>::type,
                     typename std::remove_reference<T>::type>::value, "Is required to be of same type");
-            auto current = new Entry(std::forward<T>(data));
+            auto current = new IEntry(std::forward<T>(data));
             begin:
             auto head = m_push.load();
-            if (head == m_reserved) {
-                Entry *resrved = m_reserved;
-                if (!m_push.compare_exchange_strong(resrved, current)) {
-                    goto begin;
-                }
-                m_poll.compare_exchange_strong(resrved, current);
-            } else if (head) {
-                //TODO probably race
+            if (head) {
                 Entry *nil = nullptr;
-                if (!head->prev.compare_exchange_strong(nil, current)) {
+                /* Expect previous to be null otherwise contention and reload head
+                 */
+                if (!head->previous().compare_exchange_strong(nil, current)) {
+                    /* nil is something else. If its NoEntry we can help out by...
+                     *
+                     */
+                    if (NoEntry *no = dynamic_cast<NoEntry *>(nil)) {
+                        m_push.compare_exchange_strong(head, no);
+//                        auto no_prev = no->previous().load();
+//                        if (no_prev) {
+////                            if (head->previous().compare_exchange_strong(head, nu)) {
+////                                auto kk = 0;
+////                            }
+//                            if (m_push.compare_exchange_strong(head, no)) {
+//                                auto kk = 0;
+//                            }
+//                            //delete no;
+//                        } else {
+////sp
+//                            bool res = head->previous().compare_exchange_strong(no_prev, current);
+//                            if (!res) {
+//                                goto begin;
+//                            }
+//                            return;
+//                        }
+                    }
                     goto begin;
                 }
-
+                /* swap head with current will not race with the 'head->previous' cas since
+                 * we will only get to this point if 'head->previous' is null and the previous
+                 * cas ensures that only one thread is allowed to change the value from null.
+                 *
+                 * this cas will publish the local 'current' node as the head of the push chain.
+                 */
                 bool res = m_push.compare_exchange_strong(head, current);
                 if (!res) {
-                    throw std::runtime_error{"res false"};
+                    std::cout << "cas1\n";
+                    exit(1);
+//                    throw std::runtime_error{"res false"};
                 }
             } else {
                 Entry *nil = nullptr;
                 if (!m_push.compare_exchange_strong(nil, current)) {
                     goto begin;
                 }
-                m_poll.compare_exchange_strong(nil, current);
+                if (!m_poll.compare_exchange_strong(nil, current)) {
+                    std::cout << "cas3\n";
+                    exit(3);
+                }
             }
         }
-
-//        size_t size() {
-//            size_t cnt{0};
-//            Entry_t *current = &m_push;
-//            begin:
-//            Entry *val = current->load();
-//            if (!val) {
-//                return cnt;
-//            }
-//            current = &val->prev;
-//            cnt = cnt + 1;
-//            goto begin;
-//        }
 
     public:
         /**
@@ -124,32 +185,55 @@ namespace sp {
             auto current = m_poll.load();
             // check if there is something in the queue
             if (current) {
-                auto prev = current->prev.load();
+                auto prev = current->previous().load();
                 /** check if current has no parent
                  *  if so current is the head for m_push.
                  */
-                if (!prev) {
-                    /** we push a placeholder as the m_push head
-                     *  so we can safely reclaim current allocated memory
+                if (NoEntry *no = dynamic_cast<NoEntry *>(current)) {
+                    if (prev == nullptr) {
+                        goto default_value;
+                    }
+                } else if (prev == nullptr) {
+                    /** - We push a placeholder as the m_push head
+                     *  so we can safely reclaim current allocated memory.
+                     *  - Also ensures that there is no raise when cas:ng current with prev by reserving prev.
+                     *  (if prev is null and some threading changing its value)
                      */
-                    bool ret = m_push.compare_exchange_strong(current, m_reserved);
+                    auto reserved = new NoEntry;
+                    bool ret = current->previous().compare_exchange_strong(prev, reserved);
                     if (!ret) {
+//                        delete reserved;
                         /** if the cas fails m_push is under contention
                          *  and we should just retry since current is likely
                          *  not the m_push head anymore.
                          */
                         goto begin;
+                    } else {
+                        prev = reserved;//sp
                     }
+                }
+                if (prev == nullptr) {
+                    std::cout << "prev == nullptr\n";
+                    exit(4);
                 }
 
                 bool res = m_poll.compare_exchange_strong(current, prev);
                 if (!res) {
-                    goto begin;
+                    std::cout << "cas2\n";
+                    exit(2);
+//                    goto begin;
                 }
-                T result(std::move(current->data));
-                delete current;
-                return result;
+                if (IEntry *e = dynamic_cast<IEntry *>(current)) {
+
+                    T result(std::move(e->data));
+//                delete current;
+                    return result;
+                } else {
+                    //NoEntry
+                    //delete current;
+                }
             }
+            default_value:
             return def;
         }
     };
--- collection/test/FifoTest.cpp
+++ collection/test/FifoTest.cpp
@@ -12,32 +12,50 @@ class FifoTest : public ::testing::TestWithParam<size_t> {
 
 };
 
-INSTANTIATE_TEST_CASE_P(Threads, FifoTest, ::testing::Range(size_t(1), size_t(10)));
+INSTANTIATE_TEST_CASE_P(Threads, FifoTest, ::testing::Range(size_t(1), size_t(150)));
 
 using namespace sp;
+
+void sp_print(const std::string &s) {
+    std::cout << s << "\n";
+    std::flush(std::cout);
+}
 
 TEST_F (FifoTest, test) {
 
     MpScFifo<size_t> l;
     ASSERT_EQ(0ul, l.pop(0ul));
-
+    sp_print("1");
     l.push_back(1lu);
+    sp_print("2");
     ASSERT_EQ(1ul, l.pop(0ul));
+    sp_print("3");
     ASSERT_EQ(0ul, l.pop(0ul));
+    sp_print("4");
 
     l.push_back(size_t(20));
+    sp_print("5");
     ASSERT_EQ(size_t(20), l.pop(size_t(0)));
+    sp_print("6");
     ASSERT_EQ(0ul, l.pop(0ul));
+    sp_print("7");
 
     l.push_back(1lu);
+    sp_print("8");
     l.push_back(20lu);
+    sp_print("9");
     size_t ssss(30);
     l.push_back(ssss);
+    sp_print("10");
 
     ASSERT_EQ(1lu, l.pop(0ul));
+    sp_print("11");
     ASSERT_EQ(20lu, l.pop(0ul));
+    sp_print("12");
     ASSERT_EQ(30lu, l.pop(0ul));
+    sp_print("13");
     ASSERT_EQ(0ul, l.pop(0ul));
+    sp_print("14");
 }
 
 template<typename T>
@@ -62,12 +80,15 @@ std::vector<std::future<size_t>> unnamed(const size_t max,
         std::packaged_task<size_t()> task([max, i, &barrier, &list]() {
             std::atomic_thread_fence(std::memory_order::memory_order_acquire);
             barrier.await();
-            std::cout<<"start\n";
+            std::cout << "start\n";
+            std::flush(std::cout);
             size_t cnt = i * max;
             const size_t maxs = max + cnt;
             while (cnt < maxs) {
                 list.push_back(cnt++);
             }
+            std::cout << "start-done\n";
+            std::flush(std::cout);
             return size_t(0);
         });
         v.push_back(task.get_future());
@@ -93,7 +114,7 @@ TEST_P(FifoTest, paralell_push) {
      * collect data
      */
     join(v);
-    std::cout<<"join-"<<std::endl;
+    std::cout << "join-" << std::endl;
 
     std::unordered_set<size_t> drain(items);
     const size_t max_size_t = std::numeric_limits<size_t>::max();
@@ -112,7 +133,7 @@ TEST_P(FifoTest, paralell_push) {
     }
 
 }
-
+// 80 - 80
 TEST_P(FifoTest, paralell_push_concurrent_pop) {
     const size_t max = 100000;
     const size_t threads = GetParam();
@@ -126,19 +147,23 @@ TEST_P(FifoTest, paralell_push_concurrent_pop) {
 
     auto futures = unnamed(max, threads, list, barrier);
 
-    size_t i=0;
+    size_t i = 0;
     std::unordered_set<size_t> drain(items);
     const size_t max_size_t = std::numeric_limits<size_t>::max();
 
     barrier.await();
 //    join(futures);
-
-    while (i < items){
+    uint64_t cnt(0);
+    while (i < items) {
+        if (cnt % uint64_t(1000000) == 0) {
+//            std::cout << i << "|" << cnt << "\n";
+        }
         size_t val = list.pop(max_size_t);
-        if(val != max_size_t){
+        if (val != max_size_t) {
             ASSERT_TRUE(drain.insert(val).second);
             ++i;
         }
+        ++cnt;
     }
 
     //Test that all elements are present in Fifo
