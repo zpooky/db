2432f3b160e5f3d5f3e4873e8359f1859db9a9e3
Fredrik Olsson <spooky.bender@gmail.com>
2016-12-24 11:49:31 +0100

    format

--- collection/Fifo.h
+++ collection/Fifo.h
@@ -6,98 +6,99 @@
 #define PROJECT_LIFO_H
 
 #include <atomic>
+#include <utility>
+#include <stdexcept>
 #include <type_traits>
 
 namespace sp {
 /**
  * TODO not working
  */
-    template<typename T>
-    class Fifo {
-    private:
-        struct Entry;
+template <typename T>
+class Fifo {
+private:
+  struct Entry;
 
-        struct Entry {
-            std::atomic<Entry *> previous{nullptr};
-            T data;
+  struct Entry {
+    std::atomic<Entry *> previous{nullptr};
+    T data;
 
-            explicit Entry(T &&p_data) :
-                    data{std::forward<T>(p_data)} {
+    explicit Entry(T &&p_data) : data{std::forward<T>(p_data)} {
+    }
+  };
 
-            }
-        };
+  std::atomic<Entry *> m_push;
+  std::atomic<Entry *> m_poll;
 
-        std::atomic<Entry *> m_push;
-        std::atomic<Entry *> m_poll;
-    public:
-        Fifo() :
-                m_push{nullptr},
-                m_poll{nullptr} {
+public:
+  Fifo() : m_push{nullptr}, m_poll{nullptr} {
+  }
+
+  ~Fifo() {
+  }
+
+  template <typename D>
+  void push_front(D &&data) {
+    static_assert(std::is_same<typename std::remove_reference<D>::type,
+                               typename std::remove_reference<T>::type>::value,
+                  "Is required to be of same type");
+    auto current = new Entry(std::forward<T>(data));
+    auto next = m_push.load();
+
+    while (!m_push.compare_exchange_strong(next, current))
+      ;
+
+    Entry *nil = nullptr;
+    if (next) {
+      bool b = next->previous.compare_exchange_strong(nil, current);
+      if (!b) {
+        throw std::runtime_error{"next true, b was false"};
+      }
+      //                assert(b);
+    } else {
+      bool b = m_poll.compare_exchange_strong(nil, current);
+      if (!b) {
+        throw std::runtime_error{"next false, b was false"};
+      }
+      //                assert(b);
+    }
+  }
+
+  template <typename D>
+  T pop(D &&def) {
+    Entry *current = m_poll.load();
+    do {
+    redo:
+      if (current == nullptr) {
+        /**
+         * if push head is empty
+         * empty:
+         *  the list is empty and return the supplied default
+         * non empty:
+         *  retry by loading from poll head.
+         *  this is a edge case when a concurrent push is accuring into an empty
+         * Fifo.
+         *  initially the push head is set then the poll head. the pop is
+         * performing in between those two events.
+         */
+        if (m_push.load() == nullptr) {
+          return def;
+        } else {
+          current = m_poll.load();
+          goto redo;
         }
+      }
+    } while (
+        !m_poll.compare_exchange_strong(current, current->previous.load()));
 
-        ~Fifo() {
+    Entry *pinned = current;
+    m_push.compare_exchange_strong(current, current->previous.load());
 
-        }
+    T result(std::move(pinned->data));
+    //            delete pinned;
 
-        template<typename D>
-        void push_front(D &&data) {
-            static_assert(std::is_same<typename std::remove_reference<D>::type,
-                    typename std::remove_reference<T>::type>::value, "Is required to be of same type");
-            auto current = new Entry(std::forward<T>(data));
-            auto next = m_push.load();
-
-            while (!m_push.compare_exchange_strong(next, current));
-
-            Entry *nil = nullptr;
-            if (next) {
-                bool b = next->previous.compare_exchange_strong(nil, current);
-                if (!b) {
-                    throw std::runtime_error{"next true, b was false"};
-                }
-//                assert(b);
-            } else {
-                bool b = m_poll.compare_exchange_strong(nil, current);
-                if (!b) {
-                    throw std::runtime_error{"next false, b was false"};
-                }
-//                assert(b);
-            }
-
-
-        }
-
-        template<typename D>
-        T pop(D &&def) {
-            Entry *current = m_poll.load();
-            do {
-                redo:
-                if (current == nullptr) {
-                    /**
-                     * if push head is empty
-                     * empty:
-                     *  the list is empty and return the supplied default
-                     * non empty:
-                     *  retry by loading from poll head.
-                     *  this is a edge case when a concurrent push is accuring into an empty Fifo.
-                     *  initially the push head is set then the poll head. the pop is performing in between those two events.
-                     */
-                    if (m_push.load() == nullptr) {
-                        return def;
-                    } else {
-                        current = m_poll.load();
-                        goto redo;
-                    }
-                }
-            } while (!m_poll.compare_exchange_strong(current, current->previous.load()));
-
-            Entry *pinned = current;
-            m_push.compare_exchange_strong(current, current->previous.load());
-
-            T result(std::move(pinned->data));
-//            delete pinned;
-
-            return result;
-        }
-    };
+    return result;
+  }
+};
 }
-#endif //PROJECT_LIFO_H
+#endif // PROJECT_LIFO_H
--- collection/Queue.h
+++ collection/Queue.h
@@ -5,80 +5,80 @@
 #ifndef _SP_QUEUE_H
 #define _SP_QUEUE_H
 
-#include <vector>
-#include <atomic>
-#include <mutex>
-#include <condition_variable>
 #include "Fifo.h"
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <vector>
 
 namespace sp {
-    template<typename T>
-    class Queue {
-    private:
-//        T m_default;
+template <typename T>
+class Queue {
+private:
+  //        T m_default;
 
-        std::mutex m_mutex;
-        std::condition_variable m_condition;
+  std::mutex m_mutex;
+  std::condition_variable m_condition;
 
-        std::atomic<uint32_t> m_sleeping{0};
-//        std::atomic<bool> m_waiting{false};
+  std::atomic<uint32_t> m_sleeping{0};
+  //        std::atomic<bool> m_waiting{false};
 
-        Fifo<T> m_fifo;
+  Fifo<T> m_fifo;
 
-    public:
-        Queue(){
-        }
+public:
+  Queue() {
+  }
 
-//        explicit Queue(T &&p_def) :
-//                m_default{std::forward<T>(p_def)} {
-//        }
+  //        explicit Queue(T &&p_def) :
+  //                m_default{std::forward<T>(p_def)} {
+  //        }
 
-        Queue(const Queue<T> &) = delete;
+  Queue(const Queue<T> &) = delete;
 
-//        Queue(Queue<T> &&o) :
-//                m_default{std::move(o.m_default)} {
-//
-//        }
+  //        Queue(Queue<T> &&o) :
+  //                m_default{std::move(o.m_default)} {
+  //
+  //        }
 
-//        template<typename Q>
-//        void enqueue(Q &&);
+  //        template<typename Q>
+  //        void enqueue(Q &&);
 
-        template<typename Q>
-        void enqueue(Q &&o) {
-            static_assert(std::is_same<typename std::remove_reference<Q>::type,
-                    typename std::remove_reference<T>::type>::value, "Is required to be of same type");
-            m_fifo.push_front(std::forward<T>(o));
-            if (m_sleeping.load() != uint32_t(0)) {
-                std::unique_lock<std::mutex> lck(m_mutex);
-                m_condition.notify_one();
-            }
-        }
-
-        T dequeue();
-
-        std::vector<T> drain();
-    };
-
-
-    template<typename T>
-    T Queue<T>::dequeue() {
-        auto val = nullptr;
-        if (val != nullptr) {
-            m_sleeping.fetch_add(uint32_t(1));
-            do {
-                std::unique_lock<std::mutex> lck(m_mutex);
-                m_condition.wait(lck);
-
-//                break;
-            } while (true);
-            m_sleeping.fetch_sub(1);
-        }
-//        return
+  template <typename Q>
+  void enqueue(Q &&o) {
+    static_assert(std::is_same<typename std::remove_reference<Q>::type,
+                               typename std::remove_reference<T>::type>::value,
+                  "Is required to be of same type");
+    m_fifo.push_front(std::forward<T>(o));
+    if (m_sleeping.load() != uint32_t(0)) {
+      std::unique_lock<std::mutex> lck(m_mutex);
+      m_condition.notify_one();
     }
-    template <typename T>
-    std::vector<T> Queue<T>::drain(){
-        std::vector<T> result;
-        return result;
-    }
+  }
+
+  T dequeue();
+
+  std::vector<T> drain();
+};
+
+template <typename T>
+T Queue<T>::dequeue() {
+  auto val = nullptr;
+  if (val != nullptr) {
+    m_sleeping.fetch_add(uint32_t(1));
+    do {
+      std::unique_lock<std::mutex> lck(m_mutex);
+      m_condition.wait(lck);
+
+      //                break;
+    } while (true);
+    m_sleeping.fetch_sub(1);
+  }
+  //        return
+}
+template <typename T>
+std::vector<T> Queue<T>::drain() {
+  std::vector<T> result;
+  return result;
+}
 }
 #endif
